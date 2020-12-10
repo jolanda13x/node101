2db6398dd87bba790aac9d134b366405fbf1d375
jolanda13x <jolanda13x@gmail.com>
2020-12-10 22:35:01 +0100

    node101

---
+++ CreateTodoList.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const moment = require('moment')
+
+const list = [
+    {
+      field: 'WaterPlants'
+    },
+    {
+        field: 'BuySpaghetti'
+    },
+    {
+        field: 'BuyPesto'
+    },
+  ]
