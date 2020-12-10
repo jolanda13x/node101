770b53d0a4d81bb19671f2e2ea4187ea6c584a9a
jolanda13x <jolanda13x@gmail.com>
2020-12-10 22:53:40 +0100

    node1010

--- CreateTodoList.js
+++ CreateTodoList.js
@@ -1,14 +1,24 @@
 const fs = require('fs')
-const moment = require('moment')
-
-const list = [
+const moment=require('moment')
+const todoList=[
     {
-      field: 'WaterPlants'
-    },
-    {
-        field: 'BuySpaghetti'
-    },
-    {
-        field: 'BuyPesto'
-    },
-  ]
+        text: 'Water Plants,
+        date: moment('11.12.2020, 21:50', 'DD.MM.YYYY, hh:mm'),
+        id: 1,
+        done: false
+      },
+      {
+        text: 'Buy Spaghetti',
+        date: moment('1.12.2020, 11:05', 'DD.MM.YYYY, hh:mm'),
+        id: 2,
+        done: true
+      },
+      {
+        text: 'Buy Pesto',
+        date: moment('1.12.2020, 11:05', 'DD.MM.YYYY, hh:mm'),
+        id: 3,
+        done: true
+      }
+]
+const json=JSON.stringify(todoList)
+fs.writeFileSync('todo.json',json)
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express=require('express')
+const fs=require('fs')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const app=express()
+const port=3000
+const data=fs.readFileSync('todo.json','utf8')
+const todoList=JSON.parse(data)
+
+app.get('/',(req,res) => {
+    //res.send('Hello World!')
+    res.json(todoList)
 })
-
 app.listen(port, () => {
-  console.log(`Web server listening at http://localhost:${port}`)
+    console.log(`Web server listening at http://localhost:${port}`)
 })
